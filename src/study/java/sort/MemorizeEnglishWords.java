a5f07c9f721985eefe8a778605aa18bb4c6c0c25
VVJD <hj828@kakao.com>
2024-03-12 15:37:00 +0900

    정렬 문제 풀이 중

--- src/study/java/sort/MemorizeEnglishWords.java
+++ src/study/java/sort/MemorizeEnglishWords.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -22,38 +23,31 @@ public class MemorizeEnglishWords {
 		
 		try {
 			StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-			
 			int N = Integer.parseInt(tokenizer.nextToken());
 			int M = Integer.parseInt(tokenizer.nextToken());
-			
-			HashMap<String, Integer> map = new HashMap<>();
-			String[] words = new String [N];
-			tokenizer = new StringTokenizer(reader.readLine());
-			
-			for (int i = 0; i < N; i++) { //담기				
-				words[i] = tokenizer.nextToken();
-			}
-			
-			for (int i = 0; i < words.length; i++) {
-				int num = 1;
-				
-				for (int j = 0; j < words.length; j++) {
 
-					if (map.containsKey(words[i])) {
-						map.replace(words[i], num++);
-						System.out.println(map.values());
+			HashMap<String, Integer> map = new HashMap<>();
+			
+			for (int i = 0; i < N; i++) { // 담기
+				String word = reader.readLine(); // 처음에는 배열에 담고 해당 배열을 확인하면서 반복 횟수를 계산했는데 그러면 코드가 복잡해지는 것 같다.
+
+				if (word.length() >= M) { //길이가 M을 넘는 경우만
+					if (map.containsKey(word)) { // 이미 해당 값이 키로 존재하면
+						map.replace(word, map.get(word) + 1);
+						System.out.println(map.entrySet());
+					} else {
+						map.put(word, 1);
 					}
 				}
 			}
-			
-//			String[] words = new String [N];
-//			tokenizer = new StringTokenizer(reader.readLine());
 //			
-//			for (int i = 0; i < N; i++) { //배열에 담기
-//				words[i] = tokenizer.nextToken();
+//			for (int j = 0; j < map.size(); j++) {
+//				map.
 //			}
-//			
-//			Arrays.sort(words); //알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다
+			
+			//Collections.sort(map.keySet(), (o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+
+			System.out.println(map.values());
 //			
 //			
 //			
