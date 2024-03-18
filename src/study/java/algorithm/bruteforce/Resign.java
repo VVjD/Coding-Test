08951cf3efa5ff64afe0baa3eda862cf02030bb5
VVJD <hj828@kakao.com>
2024-03-18 17:47:23 +0900

    브루트포스 알고리즘-퇴사

--- src/study/java/algorithm/bruteforce/Resign.java
+++ src/study/java/algorithm/bruteforce/Resign.java
@@ -45,28 +45,68 @@ public class Resign {
             e.printStackTrace();
         }
     } // main
-
+    
     
     
     private static int getMaxProfit(int N, List<Table> list) {
-        int[] dp = new int[N + 1]; //각 날짜까지의 최대 이익을 저장
+        int[] dp = new int[N + 1]; //각 날짜까지의 최대 이익을 저장 > 0부터 시작하기 때문에 1을 저장해서 N까지 값으로 가질 수 있게 설정
 
         for (int i = 0; i < N; i++) {
             int endDate = i + list.get(i).getPeriod(); // 상담 종료일
             
             if (endDate <= N) { // 상담 종료일이 퇴사 전인 경우
-                dp[endDate] = Math.max(dp[endDate], dp[i] + list.get(i).getMoney()); //Returns the greater of two int values. That is, theresult is the argument closer to the value of Integer.MAX_VALUE. If the arguments have the same value,the result is that same value.
-                //dp[i] + list.get(i).getMoney(): 현재 상담을 진행했을 때의 새로운 이익입니다. 이전까지의 최대 이익(dp[i])에 현재 상담을 진행했을 때의 이익을 더한 값입니다.
-                //dp[endDate]: 현재 상담 종료일(endDate)까지의 최대 이익을 나타냅니다. 이 값은 이미 이전에 계산된 값 중에서 가장 큰 이익을 나타내고 있습니다. 따라서, 현재 상담을 진행하면서 얻을 수 있는 새로운 이익과 기존의 최대 이익 중 큰 값을 선택하여 dp[endDate]에 저장합니다.
+                dp[endDate] = Math.max(dp[endDate], dp[i] + list.get(i).getMoney()); 
+                //Returns the greater of two int values. That is, theresult is the argument closer to the value of Integer.MAX_VALUE. If the arguments have the same value,the result is that same value.
+                //dp[i] + list.get(i).getMoney(): 이전까지의 최대 이익에 현재 상담을 진행했을 때의 이익을 더한 값
+                //dp[endDate]: 현재 상담 종료일까지의 최대 이익
+                //endDate가 동일한 경우 이전의 최대 이익 값과 현재 상담을 진행했을 때의 이익 값을 비교하여 최대 이익을 저장한다.
             }
             
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]); // dp[i + 1] = Math.max(dp[i + 1], dp[i]);: 다음 날짜의 최대 이익을 갱신합니다. 현재까지의 최대 이익(dp[i])과 다음 날짜의 최대 이익(dp[i + 1]) 중 큰 값을 선택하여 dp[i + 1]에 저장합니다. 이는 이전까지의 최대 이익을 현재까지의 최대 이익으로 가져오는 과정입니다.
-		//현재 경우의 수가 0일 수 있기 때문에 이전의 최대값과 비교 후 갱신.
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]); 
+            // dp[i + 1] = Math.max(dp[i + 1], dp[i]);: 다음 날짜의 최대 이익을 갱신한다. 현재까지의 최대 이익(dp[i])과 다음 날짜의 최대 이익(dp[i + 1]) 중 큰 값을 선택하여 저장한다.
+            //예를 들어 i가 4인 경우 4일에 종료되는 상담과 5일에 종료되는 상담의 이익을 비교하는데, 만약 5일에 종료되는 상담이 존재하지 않는 경우 해당 날짜의 최대 이익은 이전 날과 동일하기 때문이다.
         }
 
-        return dp[N]; //퇴사 날짜(N)까지의 최대 이익을 반환합니다. 이는 dp 배열의 마지막 요소에 해당한다.
+        return dp[N]; //퇴사 날짜(N)까지의 최대 이익을 반환한다.
     }
-}
+} //Resign
+
+	/*
+ 
+ 		i = 0
+ 		endDate = 0 + 3 = 3
+ 		dp[3] = Math.max(0, dp[2] + 10) = 10
+ 		dp[1] = Math.max(dp[1], dp[0]) = 0
+ 		
+ 		i = 1
+ 		endDtae = 1 + 5 = 6
+ 		dp[6] = Math.max(dp[6], dp[1] + 20) = 20 //2일 상담을 하는 경우 1일 상담은 할 수 없음 > 얘는 2일에 시작하는 거라 1일에 종료하는 상담의 금액에 이 강의를 진행하였을 때의 금액을 더한 값과 이전에 존재하는 상담 종료일이 일치하는 값과 비교하는 것이다.
+ 		dp[2] = Math.max(dp[2], dp[1]) = 0
+ 		
+ 		i = 2
+ 		endDate = 2 + 1 = 3
+ 		dp[3] = Math.max(10, dp[2] + 10) = 10 //3일 상담을 하는 경우 1일과 2일 상담은 할 수 없음
+ 		dp[3] = Math.max(dp[3], dp[2]) = 10 
+ 		
+ 		i = 3
+ 		endDate = 3 + 1 = 4
+ 		dp[4] = Math.max(dp[4], dp[3] + 20) = 30
+ 		dp[4] = Math.max(dp[4], dp[3]) = 30
+ 		
+ 		i = 4
+ 		endDate = 4 + 2 = 6
+ 		dp[6] = Math.max(dp[6], dp[4] + 15) = 45
+ 		dp[5] = Math.max(dp[5], dp[4]) = 30 //여기서보면 5일에 종료되는 과정이 없어서 5일에 최대 금액은 4일과 동일하게 처리한다.
+ 		
+ 		i = 5
+ 		endDate = 5 + 4 = 9
+ 		dp[6] = Math.max(dp[6], dp[5]) = 45
+ 		
+ 		i = 6
+ 		endDate = 6 + 2 = 8
+ 		dp[7] = Math.max(dp[7], dp[6]) = 45
+ 
+	 */
 
 
 
