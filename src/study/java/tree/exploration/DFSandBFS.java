ea3d1fbc5392483499c24b0a2c8316612bdd983b
VVJD <hj828@kakao.com>
2024-05-27 13:35:24 +0900

    DFS와 BFS

---
+++ src/study/java/tree/exploration/DFSandBFS.java
@@ -0,0 +1,16 @@
+package study.java.tree.exploration;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//백준 1250번, DFS와 BFS
+public class DFSandBFS {
+	public static void main(String[] args) {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+	}
+}
---
+++ src/study/java/tree/exploration/Virus.java
@@ -0,0 +1,6 @@
+package study.java.tree.exploration;
+
+//백준 2606번, 바이러스
+public class Virus {
+
+}
